5128c1c83d91a73c753437ea856a1bb7a7283e83
森口寛生 <you know@gmail.com>
2014-12-19 20:07:56 +0900

    Bon jour

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Git
+//
+//  Created by 森口寛生 on 2014/12/19.
+//  Copyright (c) 2014年 森口寛生. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
